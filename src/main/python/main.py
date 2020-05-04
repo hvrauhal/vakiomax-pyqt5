90c58f402856c10e0dda8375ba51d464a4377878
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2020-05-04 14:53:14 +0300

    Now shows something

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,37 @@
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+#!/usr/bin/env python
+
 
 import sys
 
+from PyQt5.QtWidgets import (QDialog, QComboBox, QLabel, QHBoxLayout, QGridLayout, QTextEdit)
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
+
+
+class VakioMax(QDialog):
+    def __init__(self, parent=None):
+        super(VakioMax, self).__init__(parent)
+        gameComboBox = QComboBox()
+        gameComboBox.addItems(['foo', 'bar'])
+
+        gameLabel = QLabel("&Peli:")
+        gameLabel.setBuddy(gameComboBox)
+
+        topLayout = QHBoxLayout()
+        topLayout.addWidget(gameLabel)
+        topLayout.addWidget(gameComboBox)
+
+        textEdit = QTextEdit()
+
+        mainLayout = QGridLayout()
+        mainLayout.addLayout(topLayout, 0, 0)
+        mainLayout.addWidget(textEdit, 1, 0)
+
+        self.setLayout(mainLayout)
+        self.setWindowTitle("VakioMax 4")
+
+
 if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
-    sys.exit(exit_code)
+    appctxt = ApplicationContext()
+    vakiomax = VakioMax()
+    vakiomax.show()
+    sys.exit(appctxt.app.exec_())
